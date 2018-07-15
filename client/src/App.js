18ffe3ddb2f05e9e6725d7bc164d7433e0e0cbd4
afonsofigs <afonso.figuinha@gmail.com>
2018-07-15 04:51:48 +0100

    Profile completed with github and when seeing a user link that does not exist, goes to page not found

--- client/src/App.js
+++ client/src/App.js
@@ -21,8 +21,8 @@ import AddExperience from "./components/add-credential/AddExperience";
 import AddEducation from "./components/add-credential/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
-
 import "./App.css";
+import NotFound from "./components/not-found/NotFound";
 
 //Check for token
 if (localStorage.jwtToken) {
@@ -89,6 +89,8 @@ class App extends Component {
                   component={AddEducation}
                 />
               </Switch>
+
+              <Route exact path="/not-found" component={NotFound} />
             </div>
             <Footer />
           </div>
